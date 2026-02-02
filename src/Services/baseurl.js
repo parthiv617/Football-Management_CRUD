af1d4019ecd03745e79f935910eecf074061fb5d
parthiv123 <parthiv817@gmail.com>
2026-02-02 12:21:22 +0530

    Updated

--- src/Services/baseurl.js
+++ src/Services/baseurl.js
@@ -1,3 +1,5 @@
-const baseurl = "https://football-management-server-1.onrender.com";
+const baseurl = process.env.NODE_ENV === 'production' 
+  ? "https://football-management-server-1.onrender.com"
+  : "http://localhost:3003";
 
 export default baseurl
